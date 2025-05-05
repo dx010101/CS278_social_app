94828bce25c468b200ec9adeb62742de1ec5d470
cjhawk23 <cjhawk@stanford.edu>
2025-05-05 15:09:39 -0700

    Update models.py
    User information database design

--- Social_app_demo/mysite/match/models.py
+++ Social_app_demo/mysite/match/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class UserInfo(models.Model):
+    name = models.CharField(max_length=100)
+    interest = models.CharField(max_length=300)
+    hometown = models.CharField(max_length=100, blank=True)
+    major = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+      
 # Create your models here.
